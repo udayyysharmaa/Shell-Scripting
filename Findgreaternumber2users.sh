0d3f1778b9bded6e7ce94f77f06b75eff4e00625
Uday Sharma <udaysharmaniit12345@gmail.com>
2024-08-31 22:46:59 +0530

    Find the Greater Number from 2 Users

---
+++ Findgreaternumber2users.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter the first number"
+
+read a
+
+
+echo "Enter the Second Number"
+
+read b
+
+
+if [ $a -gt $b ] then
+ehco " The Greater number is" $a
+else
+echo "The Greater Number is" $b
+fi
+
